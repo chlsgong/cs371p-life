ffcd9adaa5497223f11f720103d04edf82b215d4
joshuagutierrez <joshuagutierrez@utexas.edu>
2015-11-13 16:27:15 -0600

    implementing methods. added iterator

--- Life.c++
+++ Life.c++
@@ -3,7 +3,79 @@
 #include <sstream>
 #include <vector>
 
+#include "Life.h"
+
 using namespace std;
+
+Cell::Cell() {
+	current_state = false;
+	age = 0;
+}
+Cell::Cell(bool initial_state) {
+	current_state = initial_state;
+	age = 0;
+}
+void Cell::evolve(int index, const Life<Cell>& l) {
+	//cout << boolalpha << current_state << endl; 
+	next_state = l.inspectNeighbors(current_state, index);
+	if(next_state) {
+		++age;
+		if(age >= 2)
+			symbol = '*'; // ConwayCell evolve
+		else {
+			// FredkinCell evolve
+			if(age >= 10)
+				symbol = '+';
+			else
+				symbol = (char) age;
+		}
+	}
+	else {
+		if(age >= 2)
+			symbol = '.';
+		else
+			symbol = '-';
+	}
+}
+
+
+ConwayCell::ConwayCell() {
+	current_state = false;
+}
+ConwayCell::ConwayCell(bool initial_state) {
+	current_state = initial_state;
+}
+void ConwayCell::evolve(int index, const Life<Cell>& l) {
+	// cout << boolalpha << current_state << endl; 
+	next_state = l.inspectNeighbors(current_state, index);
+	if(next_state)
+		symbol = '*';
+	else
+		symbol = '.';
+}
+
+
+FredkinCell::FredkinCell() {
+	current_state = false;
+	age = 0;
+}
+FredkinCell::FredkinCell(bool initial_state) {
+	current_state = initial_state;
+	age = 0;
+}
+void FredkinCell::evolve(int index, const Life<Cell>& l) {
+	// cout << boolalpha << current_state << endl; 
+	next_state = l.inspectNeighbors(current_state, index);
+	if(next_state) {
+		++age;
+		if(age >= 10)
+			symbol = '+';
+		else
+			symbol = (char) age;
+	}
+	else
+		symbol = '-';
+}
 
 void enact_life(istream& r) {
 	string s;
@@ -50,6 +122,19 @@ void enact_life(istream& r) {
 		getline(r, s); // blank line
 
 		// ... code
+
+		if(cell_type == "Cell") {
+			Life<Cell> l(rows, columns, grid);
+			l.runLife(evolutions);
+		}
+		else if(cell_type == "ConwayCell") {
+			Life<ConwayCell> l(rows, columns, grid);
+			l.runLife(evolutions);
+		}
+		else if(cell_type == "FredkinCell") {
+			Life<FredkinCell> l(rows, columns, grid);
+			l.runLife(evolutions);
+		}
 
 	}
 }
--- Life.h
+++ Life.h
@@ -2,25 +2,70 @@
 #define DARWIN_H
 
 #include <iostream>
+#include <vector>
+#include <iterator>
 
 class AbstractCell {
 	protected:
-		bool alive;
+		bool current_state; // true = alive, false = dead
+		bool next_state;
+		char symbol;
 
 	public:
-		virtual void evolve(int index) = 0;
+		virtual void evolve(int) = 0;
 };
 
 class Cell: public AbstractCell {
-	void evolve(int index);
+	protected:
+		int age;
+
+	public:
+		Cell();
+		Cell(bool);
+		void evolve(int, const Life<Cell>&);
 };
 
 class ConwayCell: public Cell {
-	void evolve(int index);
+	public:
+		ConwayCell();
+		ConwayCell(bool);
+		void evolve(int, const Life<Cell>&);
 };
 
 class FredkinCell: public Cell {
-	void evolve(int index);
+	public:
+		FredkinCell();
+		FredkinCell(bool);
+		void evolve(int, const Life<Cell>&);
+};
+
+template<typename T>
+class L_Iterator {
+	private:
+		T _v;
+
+	public:
+		L_Iterator(const T& v) {
+			_v = v;
+		}
+		bool operator == (const L_Iterator<T>& rhs) const {
+			return (_v == rhs._v);
+		}
+		bool operator != (const L_Iterator<T>& rhs) const {
+			return !(*this == rhs);
+		}
+		const T& operator * () const {
+			return _v;
+		}
+		L_Iterator<T> operator ++ () { // pre increment
+			++_v;
+			return *this;
+		}
+		L_Iterator<T> operator ++ (int) { // post increment
+			L_Iterator<T> d = *this;
+			++*this;
+			return d;
+		}
 };
 
 template<typename T>
@@ -28,27 +73,27 @@ class Life {
 	private:
 		int rows;
 		int columns;
-		vector<T> grid;
+		std::vector<T> grid;
 	public:
-		Life(int r, int c, vector<char> g) {
+		Life(int r, int c, std::vector<char> g) {
 			rows = r;
 			columns = c;
 			int size = r*c;
 			for(int i = 0; i < size; i++) {
 				if(g[i] == '.') { // dead conway cell
-					T cell(false) = new ConwayCell;
+					T cell = new ConwayCell(false);
 					grid.push_back(cell);
 				}
 				else if(g[i] == '-') { // dead fredkin cell
-					T cell(false) = new FredkinCell;
+					T cell = new FredkinCell(false);
 					grid.push_back(cell);
 				}
 				else if(g[i] == '*') { // live conway cell
-					T cell(true) = new ConwayCell;
+					T cell = new ConwayCell(true);
 					grid.push_back(cell);
 				}
 				else { // live fredkin cell
-					T cell(true) = new FredkinCell;
+					T cell = new FredkinCell(true);
 					grid.push_back(cell);
 				}
 			}
@@ -56,11 +101,32 @@ class Life {
 
 		void runLife(int evolutions) {
 			// call cell.evolve(i)
+			for(int i = 0; i < evolutions; i++) {
+				L_Iterator<int> b = this->begin();
+				L_Iterator<int> e = this->end();
+				while(b != e) {
+					//std::cout << *b << std::endl;
+					//std::cout << *e << std::endl;
+					grid[*b].evolve(*b, *this);
+					++b;
+				}
+			}
 		}
 
 		bool inspectNeighbors(char state, int position) {
 			// counts alive neighbor cells
-			// return true if change is needed, false if not
+			// return true if alive, false if dead
+			return true;
+		}
+
+		L_Iterator<int> begin() {
+			L_Iterator<int> b(0);
+			return b;
+		}
+		L_Iterator<int> end() {
+			int end = rows * columns;
+			L_Iterator<int> e(end);
+			return e;
 		}
 };
 
